b9e945e9ee75e37b85c30b5c9bc4ed8a065ead29
Brianna Fisher <Bnfisher4@gmail.com>
2020-03-27 10:22:37 -0500

    adds widget form

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .models import Widget
+from .forms import WidgetForm
+
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    widgets = Widget.objects.all()
+    form = WidgetForm()
+    if request.method == 'POST':
+        form = WidgetForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'widgets': widgets, 'form':form}
+    return render(request, 'index.html', context)
